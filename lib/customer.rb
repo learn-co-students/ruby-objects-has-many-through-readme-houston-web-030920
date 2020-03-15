0e01f90c011cb668ab159b0d13e1c99fe735d08e
angeloxenakis <angeloxenakis@gmail.com>
2020-03-14 23:02:29 -0500

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,38 @@
 class Customer
-  
+    @@all = []
+    attr_accessor :name, :age
+
+    def initialize(name, age)
+        @name = name
+        @age = age 
+        @@all.push(self)
+    end 
+
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end 
+
+    def meals()
+        customer_meals = []
+        Meal.all.each do |meal|
+            if meal.customer == self
+                customer_meals.push(meal)
+            end 
+        end
+        customer_meals 
+    end 
+
+    def waiters()
+        customer_waiters = []
+        Meal.all.each do |meal|
+            if meal.customer == self
+                customer_waiters.push(meal.waiter)
+            end 
+        end
+        customer_waiters 
+    end 
+
+    def self.all()
+        @@all
+    end 
 end
